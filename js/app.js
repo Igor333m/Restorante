b94fa222f5bc5c0e210e472c980b38267c4c189c
Igor Todorovic <igor333m@yahoo.com>
2016-04-08 19:23:21 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$("button").on('click', function() {
+	console.info("Button clicked");
+});
+
+$(".slicky").slick({
+          fade : true,
+          autoplay : true,
+          autoplaySpeed : 10000,
+          arrows : false,
+          dots : true
+        });
